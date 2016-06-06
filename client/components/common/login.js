849e7352808f6f9a30e54aa24d2598dc032d11b0
uinika <uinika@163.com>
2016-06-06 14:54:08 +0800

    Fix bug

---
+++ client/components/common/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+      </div>
+    );
+  }
+};
+
+export default Login;
---
+++ client/components/dashboard/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+          dashboard
+      </div>
+    );
+  }
+};
+
+export default Index;
