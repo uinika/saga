012980067a727081eb009844481b6d951c808e7b
uinika <uinika@163.com>
2016-06-08 14:07:28 +0800

    Update config.js & support koa.js

---
+++ server/app.js
@@ -0,0 +1,26 @@
+'use strict';
+const Koa = require('koa'),
+      App = Koa(),
+      Config = require('./config'),
+      BodyParser = require('body-parser');
+
+/** CORS Filter */
+// Setting static resource for express
+// const path = require('path');
+// var client = path.resolve(__dirname + '/../client');
+// app.use(express.static(client));
+App.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', Config.cors);
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Age', 1728000);
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') res.sendStatus(200);
+  else next();
+});
+App.listen(8080);
+console.info('http-server[koa] listening on 8080');
+
+/** Login */
+var login = require('./api/common/login');
+App.use('/api', login);
