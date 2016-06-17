d0e1c7f2d21965d2d12aed02d3afd9fe00fc5306
uinika <uinika@163.com>
2016-06-17 15:47:13 +0800

    Support fetch

--- client/components/common/login.js
+++ client/components/common/login.js
@@ -5,9 +5,34 @@ import 'whatwg-fetch';
 
 class Login extends React.Component {
   componentDidMount() {
-    fetch('http://172.16.0.119:8080/cors/test').then(function(response) {
+    fetch('http://172.16.0.119:8080/cors/test', {
+      credentials: 'include',
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    }).then(function(response) {
       console.log(response);
-    })
+    });
+    fetch('http://172.16.0.119:8080/cors/test/2016', {
+      credentials: 'include',
+      method: 'GET',
+    }).then(function(response) {
+      console.log(response);
+    });
+    fetch('http://172.16.0.119:8080/cors/test', {
+      credentials: 'include',
+      method: 'PUT',
+    }).then(function(response) {
+      console.log(response);
+    });
+    fetch('http://172.16.0.119:8080/cors/test', {
+      credentials: 'include',
+      method: 'DELETE',
+    }).then(function(response) {
+      console.log(response);
+    });
   };
   render() {
     return (
