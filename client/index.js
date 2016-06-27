0bcc9d2e28d40f8265d2c28006ace54bcd155888
uinika <uinika@163.com>
2016-06-27 10:59:00 +0800

    Update react-router

--- client/index.js
+++ client/index.js
@@ -3,20 +3,18 @@ import ReactDom from 'react-dom';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import 'antd/dist/antd.min.css';
 import './styles/main.less';
+import Login from './components/common/login';
 
 ReactDom.render((
   <Router history={hashHistory}>
-    <Route path='/'>
+    <Route path='/' component={Login}>
+      <IndexRoute component={Login}/>
       <Route path='login' getComponent={(nextState, callback) => {
-        require.ensure([], (require) => {
-            callback(null, require('./components/common/login').default)
-        })
-      }} />
+        require.ensure([], (require) => {callback(null, require('./components/common/login'))})
+      }}/>
       <Route path='dashboard' getComponent={(nextState, callback) => {
-        require.ensure([], (require) => {
-          callback(null, require('./components/dashboard/main').default)
-        })
-      }} />
+        require.ensure([], (require) => {callback(null, require('./components/dashboard/main'))})
+      }}/>
     </Route>
   </Router>
 ), document.getElementById('app'));
