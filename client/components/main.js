a9681ffe496ede3d373d68e4d223779319e76231
uinika <uinika@163.com>
2016-06-28 20:50:04 +0800

    Update menuTree

--- client/components/main.js
+++ client/components/main.js
@@ -1,31 +1,30 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import {Menu, Icon} from 'antd';
+import {Path, Http} from '../utils/http';
 
 class Login extends React.Component {
   componentDidMount() {
-    fetch('http://172.16.0.119:8080/test/12345', {
-      credentials: 'include',
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + sessionStorage.token
-      }
+    Http({
+      url: '/navigation/menuTree',
+      method: 'GET'
     })
-    .then(result => result.json())
-    .then(data => console.log(data));
+    .then(data => {
+      if(data.head.status === 200){
+        console.log(data);
+      }
+    });
   };
   render() {
     return (
       <div id='dashboard-main'>
         <Menu onClick={this.handleClick} style={{width: 240}} mode='inline'>
-          <Menu.SubMenu key='sub2' title={<span><Icon type='appstore' /><span>导航二</span></span>}>
-            <Menu.Item key='5'>选项5</Menu.Item>
-            <Menu.Item key='6'>选项6</Menu.Item>
-            <Menu.SubMenu key='sub3' title='三级导航'>
-              <Menu.Item key='7'>选项7</Menu.Item>
-              <Menu.Item key='8'>选项8</Menu.Item>
+          <Menu.SubMenu key='sub2' title={<span><Icon type='appstore' /><span>导航</span></span>}>
+            <Menu.Item key='5'>选项</Menu.Item>
+            <Menu.Item key='6'>选项</Menu.Item>
+            <Menu.SubMenu key='sub3' title='二级导航'>
+              <Menu.Item key='7'>选项</Menu.Item>
+              <Menu.Item key='8'>选项</Menu.Item>
             </Menu.SubMenu>
           </Menu.SubMenu>
         </Menu>
