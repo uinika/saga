3b64749d468d4bbacee1b60d44720d98f7694eda
uinika <uinika@163.com>
2016-06-28 21:03:41 +0800

    Update mock datas

--- server/app.js
+++ server/app.js
@@ -7,9 +7,7 @@ App.use('/client', Express.static('./client'));
 App.use(BodyParser.json());
 App.listen(5002);
 console.info('Server started http://localhost:5002/client/');
+// Login
+App.use(BaseUrl, require('./mock/login'));
 // Dashboard
 App.use(BaseUrl, require('./mock/dashboard'));
-// Indicator
-App.use(BaseUrl, require('./mock/indicator'));
-// Statistics
-App.use(BaseUrl, require('./mock/statistics'));
--- server/mock/login.js
+++ server/mock/login.js
@@ -1,12 +1,12 @@
 const Router = require('express').Router(),
       Util = require('../util.js');
 /** Router definition */
-Router.route('/statistics')
+Router.route('/dashboard')
   .get(function(request, response) {
     let protocal = Util.Protocal();
     protocal.head.status = '200';
     protocal.head.message = 'http response sucess';
-    protocal.body = Util.Json('/statistics/demo.json');
+    protocal.body = Util.Json('/login.json');
     response.json(protocal);
 });
 /** Module export */
