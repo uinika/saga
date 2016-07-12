96d6e2a937333c4692c6e06f81782931ce336cb6
uinika <zhengh@chinawiserv.com>
2016-07-12 18:11:46 +0800

    Update styles for frame

--- client/components/frame/main.js
+++ client/components/frame/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import {Row, Col, Menu, Icon} from 'antd';
+import {Row, Col, Menu, Icon, Dropdown} from 'antd';
 import {Path, Http} from '../../common/http';
 import {Link} from 'react-router';
 
@@ -17,11 +17,30 @@ class Login extends React.Component {
     });
   };
   render() {
+    const menu = (
+      <Menu>
+        <Menu.Item key="0">
+          <a target="_blank" href="http://www.alipay.com/">管理员菜单项1</a>
+        </Menu.Item>
+        <Menu.Item key="1">
+          <a target="_blank" href="http://www.taobao.com/">管理员菜单项2</a>
+        </Menu.Item>
+      </Menu>
+    );
     return (
       <div id='frame-main'>
         <section className='first'>
           <span className='logo'></span>
-          <span className='avatar'></span>
+          <span className='avatar'>
+            <div className='portrait'></div>
+            <div className='dropdown'>
+              <Dropdown overlay={menu}>
+                <a className="ant-dropdown-link">
+                  管理员 <Icon type="down" />
+                </a>
+              </Dropdown>
+            </div>
+          </span>
         </section>
         <section className='second'>
           <div className='side-bar'>
